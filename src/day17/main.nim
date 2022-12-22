b7e607c1747d937ef5074c3e030e7f8355e2ff8b
Gabriel Juchault <gabriel.juchault@gmail.com>
2022-12-22 02:03:56 +0100

    feat(aoc2022): day 17, part 2

--- src/day17/main.nim
+++ src/day17/main.nim
@@ -115,7 +115,7 @@ proc makePieceStopped(cave: var Cave, nextCoordinates: seq[(int, int)]) =
   for (x, y) in nextCoordinates:
     cave.incl(x, y, CaveItem.StoppedPiece, CaveItem.Empty)
 
-proc part1*(actions: seq[Action]): int =
+proc getTowerHeight*(actions: seq[Action], maxPieces: int): (int, seq[int]) =
   var cave = Cave()
   let pieceStartingX = 3
 
@@ -124,14 +124,16 @@ proc part1*(actions: seq[Action]): int =
   var pieceOrderIndex = 0
   var pieceFalling = none(PieceFalling)
   var cycles = 0
-  var lowestBottomSolidCoordinate = 0
+  var towerHeight = 0
   var stoppedPiecesCounter = 0
+
+  var heightDiff = newSeq[int]()
 
   while true:
     # if every piece is stabilized, add a new piece
     if pieceFalling.isNone():
-      pieceFalling = some((piecesOrder[pieceOrderIndex], (pieceStartingX, lowestBottomSolidCoordinate + 4)))
-      setInitialPieceOnCave(cave, piecesOrder[pieceOrderIndex], pieceStartingX, lowestBottomSolidCoordinate + 4)
+      pieceFalling = some((piecesOrder[pieceOrderIndex], (pieceStartingX, towerHeight + 4)))
+      setInitialPieceOnCave(cave, piecesOrder[pieceOrderIndex], pieceStartingX, towerHeight + 4)
       pieceOrderIndex = (pieceOrderIndex + 1) mod piecesOrder.len
 
     let nextPieceCoordinates = getNextPieceCoordinates(cave, pieceFalling.get(), actions[cycles mod actions.len])
@@ -140,20 +142,68 @@ proc part1*(actions: seq[Action]): int =
     if nextPieceCoordinates.isLastMove == IsLastMove.Yes:
       makePieceStopped(cave, nextPieceCoordinates.nextCoordinates)
       pieceFalling = none(PieceFalling)
-      lowestBottomSolidCoordinate = max(lowestBottomSolidCoordinate, nextPieceCoordinates.maxHeight)
+      
+      let newTowerHeight = max(towerHeight, nextPieceCoordinates.maxHeight)
+      heightDiff.add(newTowerHeight - towerHeight)
+      towerHeight = newTowerHeight
+
       stoppedPiecesCounter += 1
 
-      if stoppedPiecesCounter == 2022:
+      if stoppedPiecesCounter == maxPieces:
         break
     else:
       pieceFalling = some(nextPieceCoordinates.newPieceFalling)
 
     cycles += 1
 
-  return lowestBottomSolidCoordinate
+  return (towerHeight, heightDiff)
 
-proc part2*(): int =
-  return 2
+proc part1*(actions: seq[Action]): int =
+  let (towerHeight, heightEvolution) = getTowerHeight(actions, 2022)
+
+  assert(towerHeight == heightEvolution.foldl(a + b, 0))
+
+  return towerHeight
+
+proc findIndexWhenSeqRepeats*(bigSeq: seq[int], size: int): int =
+  let cmp = bigSeq[ 0 .. size ]
+
+  for i in size .. bigSeq.len - size:
+    if bigSeq[ i .. i + size ] == cmp:
+      return i
+  
+  return -1
+
+proc part2Magic*(sample: seq[int], earlyToSkip: int, seqToCheck: int, maxPieces: int64): int64 =
+  # eliminate the early that can be unstable
+  let reducedSample = sample[ earlyToSkip .. ^1 ]
+
+  # find repeated sequence (when are the first 100 ints repeated)
+  let seqRepeatsEvery = findIndexWhenSeqRepeats(reducedSample, seqToCheck)
+
+  let seqValue = reducedSample[ 0 .. seqRepeatsEvery - 1 ].foldl(a + b, 0)
+  
+  var towerHeight: int64 = 0
+
+  # first add the skipped ones
+  towerHeight += sample[ 0 .. earlyToSkip - 1 ].foldl(a + b, 0)
+
+  # then add as much of the seq as possible
+  let timesToRepeatSeq = (maxPieces - earlyToSkip) div seqRepeatsEvery
+  towerHeight += seqValue * timesToRepeatSeq
+
+  # then finish individually
+  let timesToRepeatAfterSeq = (maxPieces - earlyToSkip) mod seqRepeatsEvery
+  for i in 0 .. timesToRepeatAfterSeq:
+    towerHeight += reducedSample[i]
+
+  return towerHeight
+
+proc part2*(actions: seq[Action]): int64 =
+  # take a sample on a couple of rounds
+  let sample = getTowerHeight(actions, 10000)[1]
+
+  return part2Magic(sample, 1000, 100, 1000000000000) - 1
 
 proc parse*(input: string): seq[Action] =
   return map(
@@ -169,7 +219,7 @@ proc day17(): void =
   let actions = parse(entireFile)
 
   echo fmt"⭐️ Part 1: {part1(actions)}"
-  echo fmt"⭐️ Part 2: {part2()}"
+  echo fmt"⭐️ Part 2: {part2(actions)}"
 
 if is_main_module:
   day17()
--- src/day17/tests.nim
+++ src/day17/tests.nim
@@ -1,12 +1,30 @@
+import std/sequtils
 import main
 import unittest
 
 let firstExample = parse(readFile("./build/firstExample.txt"))
 
+suite "findIndexWhenSeqRepeats()":
+  test "given a big seq and a repeating seq, it finds when it starts to repeat":
+    let bigSeq = @[1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2]
+
+    check(findIndexWhenSeqRepeats(bigSeq, 3) == 6)
+
 suite "part1()":
   test "given example(s), it returns the expected result(s)":
     check(part1(firstExample) == 3068)
 
+suite "part2Magic()":
+  test "given a simple example, it returns the height":
+    let expandedSeq = @[1,2, 3].concat(@[4, 5, 6, 7].cycle(30))[ 0 .. 100 ]
+
+    check(part2Magic(
+      @[1, 2, 3, 4, 5, 6, 7, 4, 5, 6, 7, 4, 5, 6, 7],
+      4,
+      3,
+      100
+    ) == expandedSeq.foldl(a + b, 0))
+
 suite "part2()":
   test "given example(s), it returns the expected result(s)":
-    check(2 == 2)
+    check(part2(firstExample) == 1514285714288)
