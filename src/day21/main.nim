749f91a3512c224d75e5a911a2700396f1b204cd
Gabriel Juchault <gabriel.juchault@gmail.com>
2023-01-01 22:14:06 +0100

    feat(aoc2022): day 21, part 2

--- src/day21/main.nim
+++ src/day21/main.nim
@@ -21,26 +21,122 @@ type Monkey* = ref object
 
 proc `$`*(m: Monkey): string =
   case m.kind:
-    of MonkeyKind.MonkeyValue: return fmt"{m.name} {m.value}"
-    of MonkeyKind.MonkeyExpression: return fmt"{m.name} {m.expression}"
+    of MonkeyKind.MonkeyValue: return fmt"{m.name}({m.value})"
+    of MonkeyKind.MonkeyExpression: return fmt"{m.name}({m.expression})"
 
-proc countYellCount(monkeys: TableRef[string, Monkey], monkeyName: string): int =
+proc countYellCount(monkeys: TableRef[string, Monkey], monkeyName: string, humnOverride: int): int =
   let monkey = monkeys[monkeyName]
+
+  if monkeyName == "humn":
+    return humnOverride
 
   case monkey.kind:
     of MonkeyKind.MonkeyValue: return monkey.value
     of MonkeyKind.MonkeyExpression:
       case monkey.expression.op:
-        of Operation.Plus: return countYellCount(monkeys, monkey.expression.left) + countYellCount(monkeys, monkey.expression.right)
-        of Operation.Minus: return countYellCount(monkeys, monkey.expression.left) - countYellCount(monkeys, monkey.expression.right)
-        of Operation.Div: return countYellCount(monkeys, monkey.expression.left) div countYellCount(monkeys, monkey.expression.right)
-        of Operation.Mult: return countYellCount(monkeys, monkey.expression.left) * countYellCount(monkeys, monkey.expression.right)
+        of Operation.Plus: return countYellCount(monkeys, monkey.expression.left, humnOverride) + countYellCount(monkeys, monkey.expression.right, humnOverride)
+        of Operation.Minus: return countYellCount(monkeys, monkey.expression.left, humnOverride) - countYellCount(monkeys, monkey.expression.right, humnOverride)
+        of Operation.Div: return countYellCount(monkeys, monkey.expression.left, humnOverride) div countYellCount(monkeys, monkey.expression.right, humnOverride)
+        of Operation.Mult: return countYellCount(monkeys, monkey.expression.left, humnOverride) * countYellCount(monkeys, monkey.expression.right, humnOverride)
+
+proc applyOppositeOperation(
+  constantMonkeyYelling: TableRef[string, int],
+  currentRootLeft: int,
+  nextMonkey: Monkey,
+  nextNextMonkey: Monkey
+): (int, Operation, int) =
+  case nextMonkey.kind:
+    of MonkeyKind.MonkeyValue: return (nextMonkey.value, Operation.Plus, 0)
+    of MonkeyKind.MonkeyExpression:
+      var choice = "left"
+      var otherValue = constantMonkeyYelling[nextMonkey.expression.left]
+
+      if nextMonkey.expression.left == nextNextMonkey.name:
+        choice = "right"
+        otherValue = constantMonkeyYelling[nextMonkey.expression.right]
+
+      case nextMonkey.expression.op:
+        of Operation.Plus: return (currentRootLeft - otherValue, Operation.Minus, otherValue)
+        of Operation.Minus:
+          # careful on sign order
+          if choice == "left":
+            return (otherValue - currentRootLeft, Operation.Minus, otherValue)
+          else:
+            return (currentRootLeft + otherValue, Operation.Plus, otherValue)
+        of Operation.Div:
+          # careful on sign order
+          if choice == "left":
+            return (otherValue div currentRootLeft, Operation.Div, otherValue)
+          else:
+            return (currentRootLeft * otherValue, Operation.Mult, otherValue)
+        of Operation.Mult: return (currentRootLeft div otherValue, Operation.Div, otherValue)
 
 proc part1*(monkeys: TableRef[string, Monkey]): int =
-  return countYellCount(monkeys, "root")
+  assert monkeys["humn"].kind == MonkeyKind.MonkeyValue
+
+  # part 1: no override for humn
+  let humnValue = monkeys["humn"].value
+
+  return countYellCount(monkeys, "root", humnValue)
 
 proc part2*(monkeys: TableRef[string, Monkey]): int =
-  return 2
+  assert monkeys["humn"].kind == MonkeyKind.MonkeyValue
+  assert monkeys["root"].kind == MonkeyKind.MonkeyExpression
+  assert monkeys["root"].expression.op == Operation.Plus
+
+  let constantMonkeyYelling = newTable[string, int]()
+
+  # cache every monkey, including the ones that depends on humn (which we won't
+  # use anyway)
+  for monkey in monkeys.keys:
+    constantMonkeyYelling[monkey] = countYellCount(monkeys, monkey, 0)
+
+  # we can see only left side is evolving when changing humn value
+  # echo countYellCount(monkeys, monkeys["root"].expression.left, 4482)
+  # echo countYellCount(monkeys, monkeys["root"].expression.left, 2000)
+  # echo countYellCount(monkeys, monkeys["root"].expression.right, 4482)
+  # echo countYellCount(monkeys, monkeys["root"].expression.right, 2000)
+
+  let rootRight = countYellCount(monkeys, monkeys["root"].expression.right, monkeys["humn"].value)
+
+  # BFS to find from rootLeft to humn by inverting all operations
+  var queue = newSeq[seq[Monkey]]()
+  var pathFromRootToHumn = newSeq[Monkey]()
+
+  queue.add(@[monkeys[monkeys["root"].expression.left]])
+
+  while queue.len > 0:
+    let path = queue.pop()
+    let lastMonkey = path[ ^1 ]
+
+    if lastMonkey.name == "humn":
+      pathFromRootToHumn = path
+      break
+    if lastMonkey.kind == MonkeyKind.MonkeyValue: continue
+
+    let leftPath = concat(path, @[monkeys[lastMonkey.expression.left]])
+    let rightPath = concat(path, @[monkeys[lastMonkey.expression.right]])
+
+    queue.add(leftPath)
+    queue.add(rightPath)
+
+  var rootLeft = rootRight
+
+  for i in 0 .. pathFromRootToHumn.len() - 2:
+    let nextMonkey = pathFromRootToHumn[i]
+    let nextNextMonkey = pathFromRootToHumn[i + 1]
+    let appliedOppositeOperation = applyOppositeOperation(
+      constantMonkeyYelling,
+      rootLeft,
+      nextMonkey,
+      nextNextMonkey
+    )
+
+    rootLeft = appliedOppositeOperation[0]
+
+  assert countYellCount(monkeys, monkeys["root"].expression.right, monkeys["humn"].value) == countYellCount(monkeys, monkeys["root"].expression.left, rootLeft)
+
+  return rootLeft
 
 proc parse*(input: string): TableRef[string, Monkey] =
   var output = newTable[string, Monkey]()
--- src/day21/tests.nim
+++ src/day21/tests.nim
@@ -17,10 +17,46 @@ lgvd: ljgn * ptdq
 drzm: hmdt - zczc
 hmdt: 32""")
 
+let minusOrderExample = parse("""root: bbbb + aaaa
+aaaa: 10
+bbbb: dddd - cccc
+cccc: 5
+dddd: humn + eeee
+eeee: 1
+humn: 0""")
+
+let invertedMinusOrderExample = parse("""root: bbbb + aaaa
+aaaa: 10
+bbbb: cccc - dddd
+cccc: 5
+dddd: humn + eeee
+eeee: 1
+humn: 0""")
+
+let divOrderExample = parse("""root: bbbb + aaaa
+aaaa: 10
+bbbb: dddd / cccc
+cccc: 5
+dddd: humn + eeee
+eeee: 1
+humn: 0""")
+
+let invertedDivOrderExample = parse("""root: bbbb + aaaa
+aaaa: 10
+bbbb: cccc / dddd
+cccc: 20
+dddd: humn + eeee
+eeee: 1
+humn: 0""")
+
 suite "part1()":
   test "given example(s), it returns the expected result(s)":
     check(part1(firstExample) == 152)
 
 suite "part2()":
   test "given example(s), it returns the expected result(s)":
+    check(part2(minusOrderExample) == 14)
+    check(part2(divOrderExample) == 49)
+    check(part2(invertedMinusOrderExample) == -6)
+    check(part2(invertedDivOrderExample) == 1)
     check(part2(firstExample) == 301)
